167235b907de6215548aa41959ff5f83341d6559
Pascal Scheiben <flohzirkus666@gmail.com>
2025-12-02 07:57:15 +0100

    Adding day1 part 1

---
+++ 2025/day01.py
@@ -0,0 +1,25 @@
+from utils.helpers import load_input
+
+
+def part1(puzzle_input: str) -> None:
+    """First puzzle solution"""
+    rotations = 0
+    dial_position = 50
+    new_position = 0
+    for row in puzzle_input.splitlines():
+        if "L" in row:
+            number = int(row.split("L")[1])
+            new_position = dial_position - number
+            dial_position = new_position % 100
+        elif "R" in row:
+            number = int(row.split("R")[1])
+            new_position = dial_position + number
+            dial_position = new_position % 100
+        if dial_position == 0:
+            rotations += 1
+    print(f"Rotations: {rotations}")
+
+
+if __name__ == "__main__":
+    puzzle1_input = load_input(input_file="input/day01_pt1.txt")
+    part1(puzzle1_input)
---
+++ 2025/utils/helpers.py
@@ -0,0 +1,4 @@
+def load_input(input_file: str) -> str:
+    with open("input/day01_pt1.txt") as f:
+        contents = f.read()
+    return contents
